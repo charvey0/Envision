fefa53b7511b692a935bff4fe991511a31dd4d86
Justin <walkerj90@icloud.com>
2021-04-01 20:15:35 -0700

    tried to fix signup page

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,9 @@ const router = require('express').Router();
 
 // const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const userRoutes = require('./api/user-routes');
 
 router.use('/', homeRoutes);
 // router.use('/api', apiRoutes);
-
+router.use('/api', userRoutes);
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const sequelize = require('./config/connection');
-const exphbs = require("express-handlebars");
+// const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 const routes = require('./controllers');
 
@@ -8,18 +8,18 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars')
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // app.use(routes);
 app.use(routes);
 
 // turn on connection to db and server
-sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
-});
+// sequelize.sync({ force: false }).then(() => {
+app.listen(PORT, () => console.log('Now listening'));
+// });
 
 // app.listen(PORT, () => console.log('Now listening'));
