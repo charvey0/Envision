747bb71d3f3ab62ce1d5c15ff4c6b19f66b3095e
Cameron Harvey <charvey0@gmail.com>
2021-04-07 01:25:20 -0700

    updated artwork views and began comment paths

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const artworkPost = require('./postArtwork')
+const artworkRoutes = require('./artwork')
 
 router.use('/users', userRoutes);
 router.use('/post', artworkPost);
+router.use('/artwork', artworkRoutes);
 
 module.exports = router;
